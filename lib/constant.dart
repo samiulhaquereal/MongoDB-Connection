811a48e25beb7e34cfb7ca32f57eda51bb7e7361
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2023-11-18 15:29:40 +0600

    1st

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,2 @@
-const USERNAME = "resoft";
-const PASSWORD = "1234567890!@#QWe";
-
-final MONGO_URL = "mongodb+srv://${Uri.encodeComponent(USERNAME)}:${Uri.encodeComponent(PASSWORD)}@demo.mbtengg.mongodb.net/DemoDataBase?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://resoft:1234567890r@demo.mbtengg.mongodb.net/DemoDataBase?retryWrites=true&w=majority";
 const COLLECTION_NAME = "users";
-
